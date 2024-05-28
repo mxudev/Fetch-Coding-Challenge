be1eae4cd5b67e8a71d8f90228d55b86b0ac0821
Jerry Xu <mxudev@gmail.com>
2024-05-28 15:39:27 -0500

    Initial Commit

---
+++ Fetch Coding Challenge/Fetch_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_ChallengeApp.swift
+//  Fetch Coding Challenge
+//
+//  Created by Jerry Xu on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
